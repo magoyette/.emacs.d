e8237138137640d5ecfca92c6616769eec4f01fe
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 14:35:08 -0400

    Configure Emacs to use UTF-8
    Use UTF-8 by default and convert CRLF to LF in Emacs settings files.

--- init.el
+++ init.el
@@ -51,5 +51,6 @@
 
 (add-to-list 'load-path "~/.emacs.d/settings/")
 
+(require 'edition-settings)
 (require 'helm-settings)
 (require 'git-settings)
---
+++ settings/edition-settings.el
@@ -0,0 +1,4 @@
+(setq buffer-file-coding-system 'utf-8-unix)
+(set-language-environment "UTF-8")
+
+(provide 'edition-settings)
