ad37d596839decbe658e082329f39eb843f1c04f
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 14:39:22 -0400

    Configure backup files

--- settings/edition-settings.el
+++ settings/edition-settings.el
@@ -1,4 +1,10 @@
 (setq buffer-file-coding-system 'utf-8-unix)
 (set-language-environment "UTF-8")
 
+;; Backup files in a backups folder
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name (concat user-emacs-directory "backups")))))
+(setq vc-make-backup-files t)
+
+
 (provide 'edition-settings)
