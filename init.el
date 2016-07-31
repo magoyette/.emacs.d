25c826c2c396a087b6117f3ce735d42fdd533a72
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 11:54:03 -0400

    Add initial init.el file

---
+++ init.el
@@ -0,0 +1,7 @@
+;;;;; Emacs Lisp settings
+
+;; Set the garbage collector to only run for each 50 MB
+(setq gc-cons-threshold 50000000)
+
+;; Prefer newest Emacs Lisp byte code
+(setq load-prefer-newer t)
