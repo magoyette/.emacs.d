9bf90fa117f13900a574e61e90aeebb7384c1920
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 23:00:12 -0400

    Configure markdown-mode to use pandoc

--- settings/markdown-settings.el
+++ settings/markdown-settings.el
@@ -1,6 +1,12 @@
 (use-package markdown-mode
   :ensure t
-  :mode (("\\.md$" . markdown-mode)
-	 ("\\.markdown$" . markdown-mode)))
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :config
+  (setq markdown-command
+	"pandoc --from markdown_github -t html5 -s")
+  (setq markdown-list-indent-width 2))
 
 (provide 'markdown-settings)
