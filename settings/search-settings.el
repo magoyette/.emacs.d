f2554939f2d152b74287474f82ecfcd9a4049f44
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 23:21:23 -0400

    Add Anzu

--- settings/search-settings.el
+++ settings/search-settings.el
@@ -4,4 +4,14 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
+;; Anzu displays the current match and the total matches for the current search
+(use-package anzu
+  :ensure t
+  :config
+  (global-anzu-mode)
+
+  ;; Use query-replace and query-replace-regexp with anzu infos
+  (global-set-key (kbd "M-%") 'anzu-query-replace)
+  (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp))
+
 (provide 'search-settings)
