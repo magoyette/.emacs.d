e8cac5cb08df9133d0ffeefd4a5d5c29953ab7c7
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-07-31 23:38:25 -0400

    Configure spaces and tabs behavior

--- settings/edition-settings.el
+++ settings/edition-settings.el
@@ -3,6 +3,12 @@
 (setq buffer-file-coding-system 'utf-8-unix)
 (set-language-environment "UTF-8")
 
+;; Insert spaces instead of tab on indent, use 4 spaces for a tab
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+(setq require-final-newline t)
+
 ;; Backup files in a backups folder
 (setq backup-directory-alist
       `(("." . ,(expand-file-name (concat user-emacs-directory "backups")))))
