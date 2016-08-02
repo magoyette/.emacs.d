4a418bdd53d37d1e74b37caac65b2344677da641
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-08-01 20:49:33 -0400

    Remove helm-command-prefix

--- settings/helm-settings.el
+++ settings/helm-settings.el
@@ -13,7 +13,7 @@
   (helm-push-mark-mode 1)
   :config
   (global-unset-key (kbd "C-x c"))
-  
+
   (setq helm-split-window-in-side-p t
         helm-move-to-line-cycle-in-source t
         helm-ff-search-library-in-sexp t
@@ -22,7 +22,7 @@
         helm-M-x-fuzzy-match t
         helm-buffers-fuzzy-matching t
         helm-recentf-fuzzy-match t)
-  
+
   (when (executable-find "curl")
     (setq helm-google-suggest-use-curl-p t))
   :bind (("C-x C-b" . helm-mini)
@@ -35,10 +35,9 @@
          ("C-x r b" . helm-filtered-bookmarks)
          ("C-h a" . helm-apropos)
          ("C-h i" . helm-info-emacs)
-         ("C-c h" . helm-command-prefix)
          :map helm-map
          ("<tab>" . helm-execute-persistent-action)
          ("C-i" . helm-execute-persistent-action)
-	 ("C-z" . helm-select-action)))
+         ("C-z" . helm-select-action)))
 
 (provide 'helm-settings)
