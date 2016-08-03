37a0ce4b83c1b50b064272f0f2e3d9e7c3e6b149
Marc-André Goyette <goyette.marcandre@gmail.com>
2016-08-02 20:19:43 -0400

    Fix broken helm-company

--- settings/company-settings.el
+++ settings/company-settings.el
@@ -1,3 +1,5 @@
+(require 'use-package)
+
 (use-package company
   :ensure t
   :diminish company-mode
@@ -15,11 +17,9 @@
 
 (use-package helm-company
   :ensure t
-  :commands helm-company
+  :after company
   :config
-  (eval-after-load 'company
-    '(progn
-       (define-key company-mode-map (kbd "C-:") 'helm-company)
-       (define-key company-active-map (kbd "C-:") 'helm-company))))
+  (define-key company-mode-map (kbd "C-:") 'helm-company)
+  (define-key company-active-map (kbd "C-:") 'helm-company))
 
 (provide 'company-settings)
