8797e0dfac37e595c28027df21a99f246255bb7b
magoyette <goyette.marcandre@gmail.com>
2017-04-28 00:04:44 -0400

    Update swaggermacs and configure swagger2-json-mode

--- settings/swagger-settings.el
+++ settings/swagger-settings.el
@@ -1,3 +1,7 @@
 (require 'swagger2-yaml-mode)
+(swagger2-yaml-mode-add-to-magic-mode-alist)
+
+(require 'swagger2-json-mode)
+(swagger2-json-mode-add-to-magic-mode-alist)
 
 (provide 'swagger-settings)
