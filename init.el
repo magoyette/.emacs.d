55e0edb61193809c613bcc19ad85e9f76a2f4e50
magoyette <goyette.marcandre@gmail.com>
2018-01-21 08:36:32 -0500

    Switch to new org mode package archive

--- init.el
+++ init.el
@@ -50,6 +50,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/") t)
 (setq package-archive-priorities
       '(("melpa" . 10)
         ("gnu" . 0)))
