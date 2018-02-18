f25218f3a218a3819321c3b77ab622831786c2a2
magoyette <goyette.marcandre@gmail.com>
2018-02-18 15:25:45 -0500

    Hack orgtbl tables to use Markdown format

--- settings/openapi-settings.el
+++ settings/openapi-settings.el
@@ -2,10 +2,22 @@
 
 (use-package flycheck-swagger-cli)
 
+;; Adapted from https://stackoverflow.com/questions/14275122/editing-markdown-pipe-tables-in-emacs
+(defun transform-org-table-in-markdown-table ()
+  "Transform an org table into a regular Markdown table."
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "--+--" nil t) (replace-match "--|--"))))
+
 (use-package openapi-yaml-mode
   :config
   (openapi-yaml-mode-add-to-magic-mode-alist)
 
-  (add-hook 'openapi-yaml-mode-hook 'turn-on-orgtbl))
+  (add-hook 'openapi-yaml-mode-hook 'turn-on-orgtbl)
+
+  (add-hook 'openapi-yaml-mode-hook
+            (lambda()
+              (add-hook 'after-save-hook 'transform-org-table-in-markdown-table
+                        nil 'make-it-local))))
 
 (provide 'openapi-settings)
