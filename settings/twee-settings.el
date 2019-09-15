f087c8248b10172babcaf8e196c49e4de41db088
magoyette <goyette.marcandre@gmail.com>
2019-09-14 22:50:24 -0400

    Upgrade to latest twee-chapbook-mode, set outline keybindings

--- settings/twee-settings.el
+++ settings/twee-settings.el
@@ -1,5 +1,12 @@
 (use-package twee-chapbook-mode
   :mode (("\\.tw$" . twee-chapbook-mode)
-         ("\\.twee$" . twee-chapbook-mode)))
+         ("\\.twee$" . twee-chapbook-mode))
+  :config
+  (define-key twee-chapbook-mode-map (kbd "TAB") 'twee-chapbook-outline-toggle)
+  (define-key twee-chapbook-mode-map (kbd "C-x n") 'twee-chapbook-narrow-to-subtree-or-widen)
+  (define-key twee-chapbook-mode-map (kbd "M-<up>") 'outline-move-subtree-up)
+  (define-key twee-chapbook-mode-map (kbd "M-<down>") 'outline-move-subtree-down)
+  (define-key twee-chapbook-mode-map (kbd "C-c C-f") 'outline-forward-same-level)
+  (define-key twee-chapbook-mode-map (kbd "C-c C-b") 'outline-backward-same-level))
 
 (provide 'twee-settings)
