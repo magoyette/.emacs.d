7ada9c5fb8401c61e5cf274ed58c7e479f15ae17
magoyette <goyette.marcandre@gmail.com>
2019-10-15 22:23:08 -0400

    Add twee-mode to configuration

--- settings/twee-settings.el
+++ settings/twee-settings.el
@@ -1,12 +1,15 @@
 (use-package twee-chapbook-mode
   :mode (("\\.tw$" . twee-chapbook-mode)
-         ("\\.twee$" . twee-chapbook-mode))
+         ("\\.twee$" . twee-chapbook-mode)))
+
+(use-package twee-mode
+  :after twee-chapbook-mode
+  :init
+  (add-hook 'twee-chapbook-mode-hook 'twee-mode)
   :config
-  (define-key twee-chapbook-mode-map (kbd "TAB") 'twee-chapbook-outline-toggle)
-  (define-key twee-chapbook-mode-map (kbd "C-x n") 'twee-chapbook-narrow-to-subtree-or-widen)
-  (define-key twee-chapbook-mode-map (kbd "M-<up>") 'outline-move-subtree-up)
-  (define-key twee-chapbook-mode-map (kbd "M-<down>") 'outline-move-subtree-down)
-  (define-key twee-chapbook-mode-map (kbd "C-c C-f") 'outline-forward-same-level)
-  (define-key twee-chapbook-mode-map (kbd "C-c C-b") 'outline-backward-same-level))
+  (define-key twee-mode-map (kbd "M-<up>") 'outline-move-subtree-up)
+  (define-key twee-mode-map (kbd "M-<down>") 'outline-move-subtree-down)
+  (define-key twee-mode-map (kbd "C-c C-f") 'outline-forward-same-level)
+  (define-key twee-mode-map (kbd "C-c C-b") 'outline-backward-same-level))
 
 (provide 'twee-settings)
